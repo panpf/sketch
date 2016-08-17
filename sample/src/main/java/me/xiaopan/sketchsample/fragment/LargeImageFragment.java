0d92c89230fa9d3b0ed238407e633582bb947df8
panpengfei <panpengfei@appchina.com>
2016-08-17 11:11:55 +0800

    Optimize the scaling of the display

--- sample/src/main/java/me/xiaopan/sketchsample/fragment/LargeImageFragment.java
+++ sample/src/main/java/me/xiaopan/sketchsample/fragment/LargeImageFragment.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright (C) 2016 Peng fei Pan <sky@xiaopan.me>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.xiaopan.sketchsample.fragment;
 
 import android.app.Activity;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 import me.xiaopan.androidinjector.InjectContentView;
 import me.xiaopan.androidinjector.InjectExtra;
@@ -61,8 +80,10 @@ public class LargeImageFragment extends MyFragment {
         imageZoomer.addOnMatrixChangeListener(new ImageZoomer.OnMatrixChangedListener() {
             @Override
             public void onMatrixChanged(ImageZoomer imageZoomer) {
-                mappingView.update(imageZoomer.getDrawableWidth(), imageZoomer.getVisibleRect());
-                scaleTextView.setText(String.valueOf(imageZoomer.getScale()));
+                RectF visibleRectF = new RectF();
+                imageZoomer.getVisibleRect(visibleRectF);
+                mappingView.update(imageZoomer.getDrawableWidth(), visibleRectF);
+                scaleTextView.setText(new DecimalFormat("##0.00").format(imageZoomer.getScale()));
             }
         });
         mappingView.getOptions().setMaxSize(300, 300);
