ae5f6c0d708177e72a67ac4e3fcc2e168cc89206
Flyge <sky@xiaopan.me>
2017-06-19 11:04:35 +0800

    :art: About page show build type and flavor

--- sample/src/main/java/me/xiaopan/sketchsample/fragment/AboutFragment.java
+++ sample/src/main/java/me/xiaopan/sketchsample/fragment/AboutFragment.java
@@ -5,7 +5,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import butterknife.BindView;
-import me.xiaopan.sketch.BuildConfig;
+import me.xiaopan.sketchsample.BuildConfig;
 import me.xiaopan.sketchsample.BaseFragment;
 import me.xiaopan.sketchsample.BindContentView;
 import me.xiaopan.sketchsample.R;
@@ -17,11 +17,14 @@ import me.xiaopan.sketchsample.R;
 public class AboutFragment extends BaseFragment {
     @BindView(R.id.text_about_versions)
     TextView versionTextView;
+    @BindView(R.id.text_about_types)
+    TextView typesTextView;
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         versionTextView.setText(getString(R.string.text_version, BuildConfig.VERSION_NAME));
+        typesTextView.setText(getString(R.string.text_types, BuildConfig.BUILD_TYPE, BuildConfig.FLAVOR));
     }
 }
