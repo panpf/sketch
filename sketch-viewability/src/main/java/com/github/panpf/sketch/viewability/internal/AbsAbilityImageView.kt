f6ee213263c29847384303a29e45b95f86870628
panpf <panpfpanpf@outlook.com>
2022-01-17 18:52:43 +0800

    change: The AbsAbilityImageView key method is not allowed to be override

--- sketch-viewability/src/main/java/com/github/panpf/sketch/viewability/internal/AbsAbilityImageView.kt
+++ sketch-viewability/src/main/java/com/github/panpf/sketch/viewability/internal/AbsAbilityImageView.kt
@@ -18,63 +18,63 @@ abstract class AbsAbilityImageView @JvmOverloads constructor(
         ViewAbilityContainerImpl(this, this)
     }
 
-    override val viewAbilityContainer: ViewAbilityContainer
+    final override val viewAbilityContainer: ViewAbilityContainer
         get() = _viewAbilityContainer
 
-    override fun superSetOnClickListener(l: OnClickListener?) {
+    final override fun superSetOnClickListener(l: OnClickListener?) {
         super.setOnClickListener(l)
         if (l == null) {
             isClickable = false
         }
     }
 
-    override fun superSetOnLongClickListener(l: OnLongClickListener?) {
+    final override fun superSetOnLongClickListener(l: OnLongClickListener?) {
         super.setOnLongClickListener(l)
         if (l == null) {
             isLongClickable = false
         }
     }
 
-    override fun getListener(): Listener<DisplayRequest, Success, Error>? {
+    final override fun getListener(): Listener<DisplayRequest, Success, Error>? {
         return _viewAbilityContainer.getRequestListener()
     }
 
-    override fun getProgressListener(): ProgressListener<DisplayRequest>? {
+    final override fun getProgressListener(): ProgressListener<DisplayRequest>? {
         return _viewAbilityContainer.getRequestProgressListener()
     }
 
-    override fun onAttachedToWindow() {
+    final override fun onAttachedToWindow() {
         super.onAttachedToWindow()
         viewAbilityContainer.onAttachedToWindow()
     }
 
-    override fun onDetachedFromWindow() {
+    final override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
         viewAbilityContainer.onDetachedFromWindow()
     }
 
-    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+    final override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
         viewAbilityContainer.onLayout(changed, left, top, right, bottom)
     }
 
-    override fun onDraw(canvas: Canvas) {
+    final override fun onDraw(canvas: Canvas) {
         viewAbilityContainer.onDrawBefore(canvas)
         super.onDraw(canvas)
         viewAbilityContainer.onDraw(canvas)
     }
 
-    override fun onDrawForeground(canvas: Canvas) {
+    final override fun onDrawForeground(canvas: Canvas) {
         viewAbilityContainer.onDrawForegroundBefore(canvas)
         super.onDrawForeground(canvas)
         viewAbilityContainer.onDrawForeground(canvas)
     }
 
-    override fun setOnClickListener(l: OnClickListener?) {
+    final override fun setOnClickListener(l: OnClickListener?) {
         viewAbilityContainer.setOnClickListener(l)
     }
 
-    override fun setOnLongClickListener(l: OnLongClickListener?) {
+    final override fun setOnLongClickListener(l: OnLongClickListener?) {
         viewAbilityContainer.setOnLongClickListener(l)
     }
 }
