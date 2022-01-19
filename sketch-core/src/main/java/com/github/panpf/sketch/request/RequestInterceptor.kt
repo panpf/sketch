d155224d429b1d71d1b588901ba90a54b7f389f3
panpf <panpfpanpf@outlook.com>
2022-01-19 23:50:08 +0800

    improve: The second generic parameter of the RequestInterceptor is now limited to ImageData

--- sketch-core/src/main/java/com/github/panpf/sketch/request/RequestInterceptor.kt
+++ sketch-core/src/main/java/com/github/panpf/sketch/request/RequestInterceptor.kt
@@ -1,11 +1,12 @@
 package com.github.panpf.sketch.request
 
 import com.github.panpf.sketch.Sketch
+import com.github.panpf.sketch.request.internal.ImageData
 import com.github.panpf.sketch.request.internal.ImageRequest
 
-interface RequestInterceptor<REQUEST : ImageRequest, RESULT> {
+interface RequestInterceptor<REQUEST : ImageRequest, DATA: ImageData> {
 
-    suspend fun intercept(chain: Chain<REQUEST, RESULT>): RESULT
+    suspend fun intercept(chain: Chain<REQUEST, DATA>): DATA
 
     interface Chain<REQUEST : ImageRequest, RESULT> {
 
