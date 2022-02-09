90df41b1b1c0af6aceb10bae94b0c9790a7b6f69
panpf <panpfpanpf@outlook.com>
2022-02-09 18:57:35 +0800

    test: Test ResizeTransformed

--- sketch/src/main/java/com/github/panpf/sketch/decode/resize/ResizeTransformed.kt
+++ sketch/src/main/java/com/github/panpf/sketch/decode/resize/ResizeTransformed.kt
@@ -7,11 +7,12 @@ class ResizeTransformed constructor(val resize: Resize) : Transformed {
     override val key: String by lazy {
         val newSize = resize.newSize
         val precisionDecider = resize.precisionDecider
+        val precisionDeciderString = precisionDecider.toString().replace("PrecisionDecider", "")
         val scale = resize.scale
         if (newSize is RealNewSize) {
-            "ResizeTransformed(${newSize.size.width}x${newSize.size.height},${precisionDecider},${scale})"
+            "ResizeTransformed(${newSize.size.width}x${newSize.size.height},${precisionDeciderString},${scale})"
         } else {
-            "ResizeTransformed(${newSize},${precisionDecider},${scale})"
+            "ResizeTransformed(${newSize},${precisionDeciderString},${scale})"
         }
     }
     override val cacheResultToDisk: Boolean = true
