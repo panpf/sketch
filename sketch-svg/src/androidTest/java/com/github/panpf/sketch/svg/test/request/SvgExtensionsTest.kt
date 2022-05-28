2075e060b2f6999f81fc098a6408885689c4d286
panpf <panpfpanpf@outlook.com>
2022-05-29 05:00:45 +0800

    test: Added unit test for svg extensions method

--- sketch-svg/src/androidTest/java/com/github/panpf/sketch/svg/test/request/SvgExtensionsTest.kt
+++ sketch-svg/src/androidTest/java/com/github/panpf/sketch/svg/test/request/SvgExtensionsTest.kt
@@ -1,6 +1,72 @@
 package com.github.panpf.sketch.svg.test.request
 
-//@RunWith(AndroidJUnit4::class)
+import android.graphics.Color
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.github.panpf.sketch.fetch.newAssetUri
+import com.github.panpf.sketch.request.DisplayRequest
+import com.github.panpf.sketch.request.ImageOptions
+import com.github.panpf.sketch.request.ImageRequest
+import com.github.panpf.sketch.request.ImageRequest.Builder
+import com.github.panpf.sketch.request.LoadRequest
+import com.github.panpf.sketch.request.svgBackgroundColor
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
 class SvgExtensionsTest {
-    // todo Write test cases
+
+    @Test
+    fun testSvgBackgroundColor() {
+        val context = InstrumentationRegistry.getInstrumentation().context
+
+        (DisplayRequest(context, newAssetUri("sample.svg")) as ImageRequest).apply {
+            Assert.assertNull(svgBackgroundColor)
+        }
+        (DisplayRequest(context, newAssetUri("sample.svg")) {
+            (this as Builder).svgBackgroundColor(Color.BLUE)
+        } as ImageRequest).apply {
+            Assert.assertEquals(Color.BLUE, svgBackgroundColor)
+        }
+
+        DisplayRequest(context, newAssetUri("sample.svg")).apply {
+            Assert.assertNull(svgBackgroundColor)
+        }
+        DisplayRequest(context, newAssetUri("sample.svg")) {
+            svgBackgroundColor(Color.BLUE)
+        }.apply {
+            Assert.assertEquals(Color.BLUE, svgBackgroundColor)
+        }
+
+        LoadRequest(context, newAssetUri("sample.svg")).apply {
+            Assert.assertNull(svgBackgroundColor)
+        }
+        LoadRequest(context, newAssetUri("sample.svg")) {
+            svgBackgroundColor(Color.BLUE)
+        }.apply {
+            Assert.assertEquals(Color.BLUE, svgBackgroundColor)
+        }
+
+        ImageOptions().apply {
+            Assert.assertNull(svgBackgroundColor)
+        }
+        ImageOptions {
+            svgBackgroundColor(Color.BLUE)
+        }.apply {
+            Assert.assertEquals(Color.BLUE, svgBackgroundColor)
+        }
+
+        val key1 = LoadRequest(context, newAssetUri("sample.svg")).key
+        val key2 = LoadRequest(context, newAssetUri("sample.svg")) {
+            svgBackgroundColor(Color.BLUE)
+        }.key
+        Assert.assertNotEquals(key1, key2)
+
+        val cacheKey1 = LoadRequest(context, newAssetUri("sample.svg")).cacheKey
+        val cacheKey2 = LoadRequest(context, newAssetUri("sample.svg")) {
+            svgBackgroundColor(Color.BLUE)
+        }.cacheKey
+        Assert.assertNotEquals(cacheKey1, cacheKey2)
+    }
 }
--- sketch-svg/src/main/java/com/github/panpf/sketch/request/SvgExtensions.kt
+++ sketch-svg/src/main/java/com/github/panpf/sketch/request/SvgExtensions.kt
@@ -4,20 +4,43 @@ import androidx.annotation.ColorInt
 
 private const val SVG_BACKGROUND_COLOR_KEY = "sketch#svgBackgroundColor"
 
-fun LoadRequest.Builder.svgBackgroundColor(@ColorInt color: Int) = apply {
+/**
+ * Set the background color of the SVG image, the default is transparent
+ */
+fun ImageRequest.Builder.svgBackgroundColor(@ColorInt color: Int): ImageRequest.Builder = apply {
     setParameter(SVG_BACKGROUND_COLOR_KEY, color)
 }
 
-fun DisplayRequest.Builder.svgBackgroundColor(@ColorInt color: Int) = apply {
+/**
+ * Set the background color of the SVG image, the default is transparent
+ */
+fun DisplayRequest.Builder.svgBackgroundColor(@ColorInt color: Int): DisplayRequest.Builder =
+    apply {
+        setParameter(SVG_BACKGROUND_COLOR_KEY, color)
+    }
+
+/**
+ * Set the background color of the SVG image, the default is transparent
+ */
+fun LoadRequest.Builder.svgBackgroundColor(@ColorInt color: Int): LoadRequest.Builder = apply {
     setParameter(SVG_BACKGROUND_COLOR_KEY, color)
 }
 
+/**
+ * Get the background color of the SVG image
+ */
+val ImageRequest.svgBackgroundColor: Int?
+    get() = parameters?.value<Int>(SVG_BACKGROUND_COLOR_KEY)
+
+/**
+ * Set the background color of the SVG image, the default is transparent
+ */
 fun ImageOptions.Builder.svgBackgroundColor(@ColorInt color: Int) = apply {
     setParameter(SVG_BACKGROUND_COLOR_KEY, color)
 }
 
-val ImageRequest.svgBackgroundColor: Int?
-    get() = parameters?.value<Int>(SVG_BACKGROUND_COLOR_KEY)
-
+/**
+ * Get the background color of the SVG image
+ */
 val ImageOptions.svgBackgroundColor: Int?
     get() = parameters?.value<Int>(SVG_BACKGROUND_COLOR_KEY)
