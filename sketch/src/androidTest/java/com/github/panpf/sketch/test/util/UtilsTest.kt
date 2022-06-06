2da8d58483f0782c2f56a1f842c3664a09ca35c0
panpf <panpfpanpf@outlook.com>
2022-06-06 23:12:26 +0800

    test: Added unit test

--- sketch/src/androidTest/java/com/github/panpf/sketch/test/util/UtilsTest.kt
+++ sketch/src/androidTest/java/com/github/panpf/sketch/test/util/UtilsTest.kt
@@ -1,6 +1,117 @@
 package com.github.panpf.sketch.test.util
 
-//@RunWith(AndroidJUnit4::class)
+import android.content.Context
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.Lifecycle.State.STARTED
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.github.panpf.sketch.test.utils.TestActivity
+import com.github.panpf.sketch.util.awaitStarted
+import com.github.panpf.sketch.util.getLifecycle
+import com.github.panpf.sketch.util.getMimeTypeFromUrl
+import com.github.panpf.sketch.util.isMainThread
+import com.github.panpf.sketch.util.requiredMainThread
+import com.github.panpf.sketch.util.requiredWorkThread
+import com.github.panpf.tools4a.test.ktx.getActivitySync
+import com.github.panpf.tools4a.test.ktx.launchActivity
+import com.github.panpf.tools4j.test.ktx.assertThrow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
 class UtilsTest {
-    // todo Write test cases
+
+    @Test
+    fun testIsMainThread() {
+        Assert.assertFalse(isMainThread())
+        Assert.assertTrue(runBlocking(Dispatchers.Main) {
+            isMainThread()
+        })
+    }
+
+    @Test
+    fun testRequiredMainThread() {
+        assertThrow(IllegalStateException::class) {
+            requiredMainThread()
+        }
+        runBlocking(Dispatchers.Main) {
+            requiredMainThread()
+        }
+    }
+
+    @Test
+    fun testRequiredWorkThread() {
+        requiredWorkThread()
+
+        assertThrow(IllegalStateException::class) {
+            runBlocking(Dispatchers.Main) {
+                requiredWorkThread()
+            }
+        }
+    }
+
+    @Test
+    fun testGetLifecycle() {
+        val context = InstrumentationRegistry.getInstrumentation().context
+        Assert.assertNull(context.getLifecycle())
+
+        val activity = TestActivity::class.launchActivity().getActivitySync()
+        Assert.assertNotNull((activity as Context).getLifecycle())
+    }
+
+    @Test
+    fun testAwaitStarted() {
+        val lifecycleOwner = object : LifecycleOwner {
+            private var lifecycle: Lifecycle? = null
+            override fun getLifecycle(): Lifecycle {
+                return lifecycle ?: LifecycleRegistry(this).apply {
+                    lifecycle = this
+                }
+            }
+        }
+        val myLifecycle = lifecycleOwner.lifecycle as LifecycleRegistry
+
+        var state = ""
+        runBlocking {
+            async(Dispatchers.Main) {
+                state = "waiting"
+                myLifecycle.awaitStarted()
+                state = "started"
+            }
+            delay(10)
+            Assert.assertEquals("waiting", state)
+            delay(10)
+            Assert.assertEquals("waiting", state)
+            runBlocking(Dispatchers.Main) {
+                myLifecycle.currentState = STARTED
+            }
+            delay(10)
+            Assert.assertEquals("started", state)
+
+            state = ""
+            async(Dispatchers.Main) {
+                state = "waiting"
+                myLifecycle.awaitStarted()
+                state = "started"
+            }
+            delay(10)
+            Assert.assertEquals("started", state)
+        }
+    }
+
+    @Test
+    fun testGetMimeTypeFromUrl() {
+        Assert.assertEquals("image/jpeg", getMimeTypeFromUrl("http://sample.com/sample.jpeg"))
+        Assert.assertEquals(
+            "image/png",
+            getMimeTypeFromUrl("http://sample.com/sample.png#path?name=david")
+        )
+    }
 }
