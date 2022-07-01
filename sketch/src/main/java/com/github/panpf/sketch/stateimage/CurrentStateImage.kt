7562927e622fe69cd5afd1980373b1931abce9c0
panpf <panpfpanpf@outlook.com>
2022-07-01 17:05:44 +0800

    new: Added CurrentStateImage

---
+++ sketch/src/main/java/com/github/panpf/sketch/stateimage/CurrentStateImage.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2019 panpf <panpfpanpf@outlook.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.panpf.sketch.stateimage
+
+import android.graphics.drawable.Drawable
+import com.github.panpf.sketch.Sketch
+import com.github.panpf.sketch.request.ImageRequest
+import com.github.panpf.sketch.target.ViewDisplayTarget
+import com.github.panpf.sketch.util.SketchException
+import com.github.panpf.sketch.util.asOrNull
+
+class CurrentStateImage : StateImage {
+
+    override fun getDrawable(
+        sketch: Sketch,
+        request: ImageRequest,
+        exception: SketchException?
+    ): Drawable? = request.target.asOrNull<ViewDisplayTarget<*>>()?.drawable
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is CurrentStateImage) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+
+    override fun toString(): String {
+        return "DrawableStateImage"
+    }
+}
