365600fa14709dcdfee768cd131d5a32ce1512c7
panpf <panpfpanpf@outlook.com>
2022-07-07 14:27:14 +0800

    test: Improve unit test

--- sketch/src/androidTest/java/com/github/panpf/sketch/test/android/ResourcesTest.kt
+++ sketch/src/androidTest/java/com/github/panpf/sketch/test/android/ResourcesTest.kt
@@ -1,6 +1,7 @@
 package com.github.panpf.sketch.test.android
 
 import android.graphics.drawable.BitmapDrawable
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.panpf.sketch.test.utils.getTestContext
 import org.junit.Assert
@@ -11,21 +12,56 @@ import org.junit.runner.RunWith
 class ResourcesTest {
 
     @Test
-    fun test() {
+    fun testBitmapDrawableBitmap() {
         val context = getTestContext()
 
-        val drawable1 =
-            context.resources.getDrawable(android.R.drawable.ic_delete) as BitmapDrawable
-        val drawable2 =
-            context.resources.getDrawable(android.R.drawable.ic_delete) as BitmapDrawable
+        val drawable1 = AppCompatResources.getDrawable(
+            context, android.R.drawable.ic_delete,
+        ) as BitmapDrawable
+        val drawable2 = AppCompatResources.getDrawable(
+            context, android.R.drawable.ic_delete,
+        ) as BitmapDrawable
 
         Assert.assertNotSame(drawable1, drawable2)
         Assert.assertSame(drawable1.bitmap, drawable2.bitmap)
 
         drawable2.bitmap.recycle()
         Assert.assertTrue(drawable1.bitmap.isRecycled)
-        val drawable3 =
-            context.resources.getDrawable(android.R.drawable.ic_delete) as BitmapDrawable
+        val drawable3 = AppCompatResources.getDrawable(
+            context, android.R.drawable.ic_delete,
+        ) as BitmapDrawable
         Assert.assertTrue(drawable3.bitmap.isRecycled)
+    }
+
+    @Test
+    fun testBitmapDrawableMutate() {
+        val context = getTestContext()
+
+        val drawable1 = AppCompatResources.getDrawable(
+            context, android.R.drawable.ic_delete,
+        ) as BitmapDrawable
+        val drawable2 = AppCompatResources.getDrawable(
+            context, android.R.drawable.ic_delete
+        ) as BitmapDrawable
+
+        Assert.assertNotSame(drawable1, drawable2)
+        Assert.assertNotSame(drawable1.paint, drawable2.paint)
+        Assert.assertEquals(255, drawable1.alpha)
+        Assert.assertEquals(255, drawable2.alpha)
+
+        val drawable3 = drawable1.mutate() as BitmapDrawable
+        Assert.assertSame(drawable1, drawable3)
+        Assert.assertSame(drawable1.paint, drawable3.paint)
+        Assert.assertEquals(255, drawable3.alpha)
+
+        drawable3.alpha = 100
+        Assert.assertEquals(100, drawable1.alpha)
+        Assert.assertEquals(255, drawable2.alpha)
+        Assert.assertEquals(100, drawable3.alpha)
+
+        val drawable4 = AppCompatResources.getDrawable(
+            context, android.R.drawable.ic_delete
+        ) as BitmapDrawable
+        Assert.assertEquals(255, drawable4.alpha)
     }
 }
