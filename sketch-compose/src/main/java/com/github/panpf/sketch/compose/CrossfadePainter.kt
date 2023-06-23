038685273a68524530ca286fbe95d27209c7a466
panpf <panpfpanpf@outlook.com>
2023-06-24 00:29:36 +0800

    fix: Fixed a bug where AsyncImage() crashed when placeholder and error were not set and the image failed to load (#184)

--- sketch-compose/src/main/java/com/github/panpf/sketch/compose/CrossfadePainter.kt
+++ sketch-compose/src/main/java/com/github/panpf/sketch/compose/CrossfadePainter.kt
@@ -81,8 +81,8 @@ internal class CrossfadePainter(
     }
 
     private fun computeIntrinsicSize(): Size {
-        val startSize = start?.intrinsicSize ?: Size.Zero
-        val endSize = end?.intrinsicSize ?: Size.Zero
+        val startSize = start?.intrinsicSize ?: Size.Unspecified
+        val endSize = end?.intrinsicSize ?: Size.Unspecified
 
         val isStartSpecified = startSize.isSpecified
         val isEndSpecified = endSize.isSpecified
