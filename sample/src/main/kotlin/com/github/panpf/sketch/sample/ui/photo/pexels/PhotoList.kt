c85e56ed58c0f85b2cd5de1a465f4814ef52cd4b
panpf <panpfpanpf@outlook.com>
2023-12-04 18:29:25 +0800

    sample: Improve the compose part in the sample App

--- sample/src/main/kotlin/com/github/panpf/sketch/sample/ui/photo/pexels/PhotoList.kt
+++ sample/src/main/kotlin/com/github/panpf/sketch/sample/ui/photo/pexels/PhotoList.kt
@@ -28,16 +28,28 @@ import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.github.panpf.sketch.datasource.DataFrom
 import com.github.panpf.sketch.request.DisplayRequest
 import com.github.panpf.sketch.request.PauseLoadWhenScrollingDrawableDecodeInterceptor
 import com.github.panpf.sketch.sample.R
@@ -75,7 +87,6 @@ fun PhotoListContent(
     }
     val context = LocalContext.current
     val localView = LocalView.current
-    // todo from subscript
     LaunchedEffect(key1 = reloadFlow) {
         scope.launch {
             restartImageFlow.collect {
@@ -141,11 +152,49 @@ fun PhotoContent(
     animatedPlaceholder: Boolean = false,
     onClick: (photo: Photo, index: Int) -> Unit
 ) {
+    var componentSize by remember { mutableStateOf(IntSize.Zero) }
+    val density = LocalDensity.current
+    val path by remember(componentSize) {
+        derivedStateOf {
+            Path().apply {
+                val viewWidth = componentSize.width.toFloat()
+                val paddingRight = 0
+                val paddingTop = 0
+                val realSize = with(density) { 20.dp.toPx() }
+                moveTo(
+                    viewWidth - paddingRight - realSize,
+                    paddingTop.toFloat()
+                )
+                lineTo(
+                    viewWidth - paddingRight.toFloat(),
+                    paddingTop.toFloat()
+                )
+                lineTo(
+                    viewWidth - paddingRight.toFloat(),
+                    paddingTop.toFloat() + realSize
+                )
+                close()
+            }
+        }
+    }
+    var dataFrom by remember { mutableStateOf<DataFrom?>(null) }
+
     val modifier = Modifier
         .fillMaxWidth()
         .aspectRatio(1f)
         .clickable {
             onClick(photo, index)
+        }
+        .onSizeChanged {
+            componentSize = it
+        }
+        .drawWithContent {
+            drawContent()
+            val dataFrom1 = dataFrom
+            if (dataFrom1 != null) {
+                val color = Color(dataFrom2Color(dataFrom1))
+                drawPath(path, color)
+            }
         }
     val request = DisplayRequest(LocalContext.current, photo.listThumbnailUrl) {
         setApplySettings(LIST)
@@ -161,6 +210,15 @@ fun PhotoContent(
         }
         crossfade()
         resizeApplyToDrawable()
+        // todo 加了 listener 会导致图片在列表滑动时不停的加载，估计是 equals 的原因
+//        listener(
+//            onSuccess = { _, result ->
+//                dataFrom = result.dataFrom
+//            },
+//            onError = { _, _ ->
+//                dataFrom = null
+//            }
+//        )
     }
     when (index % 3) {
         0 -> {
@@ -191,5 +249,22 @@ fun PhotoContent(
                 contentDescription = "photo"
             )
         }
+    }
+}
+
+private const val FROM_FLAG_COLOR_MEMORY = 0x77008800   // dark green
+private const val FROM_FLAG_COLOR_MEMORY_CACHE = 0x7700FF00   // green
+private const val FROM_FLAG_COLOR_RESULT_CACHE = 0x77FFFF00 // yellow
+private const val FROM_FLAG_COLOR_LOCAL = 0x771E90FF   // dodger blue
+private const val FROM_FLAG_COLOR_DOWNLOAD_CACHE = 0x77FF8800 // dark yellow
+private const val FROM_FLAG_COLOR_NETWORK = 0x77FF0000  // red
+fun dataFrom2Color(dataFrom: DataFrom): Int {
+    return when (dataFrom) {
+        DataFrom.MEMORY_CACHE -> FROM_FLAG_COLOR_MEMORY_CACHE
+        DataFrom.MEMORY -> FROM_FLAG_COLOR_MEMORY
+        DataFrom.RESULT_CACHE -> FROM_FLAG_COLOR_RESULT_CACHE
+        DataFrom.DOWNLOAD_CACHE -> FROM_FLAG_COLOR_DOWNLOAD_CACHE
+        DataFrom.LOCAL -> FROM_FLAG_COLOR_LOCAL
+        DataFrom.NETWORK -> FROM_FLAG_COLOR_NETWORK
     }
 }
