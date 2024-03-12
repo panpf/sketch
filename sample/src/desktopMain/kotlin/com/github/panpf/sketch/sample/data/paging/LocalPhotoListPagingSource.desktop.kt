7e3b725ae380f76451e7b45d8d7c3573727a7b14
panpf <panpfpanpf@outlook.com>
2024-03-12 18:29:32 +0800

    sample: The desktop version of the demo app supports reading pictures in the user's Pictures directory

--- sample/src/desktopMain/kotlin/com/github/panpf/sketch/sample/data/paging/LocalPhotoListPagingSource.desktop.kt
+++ sample/src/desktopMain/kotlin/com/github/panpf/sketch/sample/data/paging/LocalPhotoListPagingSource.desktop.kt
@@ -8,12 +8,33 @@ import com.github.panpf.sketch.request.ImageRequest
 import com.github.panpf.sketch.sample.appSettings
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.File
 
 actual suspend fun readPhotosFromPhotoAlbum(
     context: PlatformContext,
     startPosition: Int,
     pageSize: Int
-): List<String> = emptyList()
+): List<String> {
+    val userHomeDir = File(System.getProperty("user.home"))
+    val userPicturesDir = File(userHomeDir, "Pictures")
+    val photoList = mutableListOf<String>()
+    var index = -1
+    userPicturesDir.walkTopDown().forEach { file ->
+        if (file.isFile) {
+            val extension = file.extension
+            if (extension == "jpeg" || extension == "jpg" || extension == "png" || extension == "gif") {
+                index++
+                if (index >= startPosition && index < startPosition + pageSize) {
+                    photoList.add(file.path)
+                }
+                if (photoList.size >= pageSize) {
+                    return@forEach
+                }
+            }
+        }
+    }
+    return photoList
+}
 
 actual fun isIgnoreExifOrientation(context: PlatformContext): Boolean {
     return context.appSettings.ignoreExifOrientation.value
