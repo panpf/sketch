4d1764887845f2e2384a7beda1a81c6e70466761
panpf <panpfpanpf@outlook.com>
2024-03-27 16:56:21 +0800

    sample: Add title bar to ios sample app

--- sample/src/iosMain/kotlin/com/github/panpf/sketch/sample/ui/gallery/PhotoListScreen.ios.kt
+++ sample/src/iosMain/kotlin/com/github/panpf/sketch/sample/ui/gallery/PhotoListScreen.ios.kt
@@ -1,6 +1,14 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.github.panpf.sketch.sample.ui.gallery
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -8,5 +16,11 @@ actual val gridCellsMinSize: Dp = 100.dp
 
 @Composable
 actual fun PhotoListHeader() {
-
+    Box(modifier = Modifier.fillMaxWidth()) {
+        TopAppBar(
+            title = {
+                Text(text = "Sketch3")
+            },
+        )
+    }
 }
