1742ede9118c36616a4597fe1e08e4780e2113c3
panpf <panpfpanpf@outlook.com>
2024-08-05 11:40:44 +0800

    new: Added startWithLifecycle() for Animatable Painter and Drawable

--- sketch-compose-core/src/commonMain/kotlin/com/github/panpf/sketch/painter/AnimatablePainter.kt
+++ sketch-compose-core/src/commonMain/kotlin/com/github/panpf/sketch/painter/AnimatablePainter.kt
@@ -1,5 +1,12 @@
 package com.github.panpf.sketch.painter
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+
 /**
  * Interface that painters supporting animations should implement.
  */
@@ -20,4 +27,28 @@ interface AnimatablePainter {
      * @return True if the animation is running, false otherwise.
      */
     fun isRunning(): Boolean
+}
+
+/**
+ * Starts the animation when the lifecycle is in [Lifecycle.Event.ON_START] state and stops it when the lifecycle is in [Lifecycle.Event.ON_STOP] state.
+ */
+@Composable
+fun AnimatablePainter.startWithLifecycle() {
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    val animatablePainter =
+        remember(this) { this } // SkiaAnimatedImagePainter needs to trigger onRemembered
+    DisposableEffect(animatablePainter) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_START) {
+                animatablePainter.start()
+            } else if (event == Lifecycle.Event.ON_STOP) {
+                animatablePainter.stop()
+            }
+        }
+        // if the LifecycleOwner is in [State.STARTED] state, the given observer * will receive [Event.ON_CREATE], [Event.ON_START] events.
+        lifecycle.addObserver(observer)
+        onDispose {
+            lifecycle.removeObserver(observer)
+        }
+    }
 }
