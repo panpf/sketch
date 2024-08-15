a6a2f6957e5a7c0bdbc7d4084b6ad987503ad0c9
panpf <panpfpanpf@outlook.com>
2024-08-15 10:30:56 +0800

    build: Improve bash scripts

--- compose_reports.sh
+++ compose_reports.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
 ./gradlew clean :sketch-compose:assembleRelease -PcomposeCompilerReports=true
--- publish_to_local.sh
+++ publish_to_local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Print error information to facilitate troubleshooting
+# Exit immediately if a command exits with a non-zero status.
 set -e
 
 # Build and install the artifacts locally to 'mavenLocal'.
