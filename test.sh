ecb3524adc2a2e33c7a2770488a2c4fe66dd155b
panpf <panpfpanpf@outlook.com>
2024-08-22 15:05:05 +0800

    test: Add test script

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+./test_desktop.sh
+./test_js.sh
+./test_wasmJs.sh
+./test_android.sh
+./test_ios.sh
---
+++ test_js.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+./gradlew jsBrowserTest
