bacc7b51866024724bc709c8495623e1aefb60fa
panpf <panpfpanpf@outlook.com>
2024-10-03 02:57:36 +0800

    test: Added unit tests for sketch-compose-resources module

--- sketch-compose-resources/src/commonTest/kotlin/com/github/panpf/sketch/compose/resources/common/test/state/PainterStateImageComposeResourcesTest.kt
+++ sketch-compose-resources/src/commonTest/kotlin/com/github/panpf/sketch/compose/resources/common/test/state/PainterStateImageComposeResourcesTest.kt
@@ -1,11 +1,27 @@
 package com.github.panpf.sketch.compose.resources.common.test.state
 
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.runComposeUiTest
+import com.github.panpf.sketch.painter.equitablePainterResource
+import com.github.panpf.sketch.state.PainterStateImage
+import com.github.panpf.sketch.state.rememberPainterStateImage
+import com.github.panpf.sketch.test.utils.compose.core.resources.Res
+import com.github.panpf.sketch.test.utils.compose.core.resources.moon
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
+@OptIn(ExperimentalTestApi::class)
 class PainterStateImageComposeResourcesTest {
 
     @Test
     fun testRememberPainterStateImage() {
-        // TODO test
+        runComposeUiTest {
+            setContent {
+                assertEquals(
+                    expected = PainterStateImage(equitablePainterResource(Res.drawable.moon)),
+                    actual = rememberPainterStateImage(Res.drawable.moon)
+                )
+            }
+        }
     }
 }
