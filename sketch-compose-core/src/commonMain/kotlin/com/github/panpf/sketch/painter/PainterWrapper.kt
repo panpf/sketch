a09010043861dc108d452d6acda0786c0eed042b
panpf <panpfpanpf@outlook.com>
2024-10-10 16:51:04 +0800

    improve: PainterWrapper overrides equals and hashCode

--- sketch-compose-core/src/commonMain/kotlin/com/github/panpf/sketch/painter/PainterWrapper.kt
+++ sketch-compose-core/src/commonMain/kotlin/com/github/panpf/sketch/painter/PainterWrapper.kt
@@ -49,4 +49,20 @@ open class PainterWrapper(val painter: Painter) : Painter() {
             draw(size, alpha, colorFilter)
         }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+        other as PainterWrapper
+        if (painter != other.painter) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return painter.hashCode()
+    }
+
+    override fun toString(): String {
+        return "PainterWrapper(painter=${painter.toLogString()})"
+    }
 }
--- sketch-compose-core/src/commonTest/kotlin/com/github/panpf/sketch/compose/core/common/test/painter/PainterWrapperTest.kt
+++ sketch-compose-core/src/commonTest/kotlin/com/github/panpf/sketch/compose/core/common/test/painter/PainterWrapperTest.kt
@@ -7,6 +7,7 @@ import com.github.panpf.sketch.painter.PainterWrapper
 import com.github.panpf.sketch.test.utils.SizeColorPainter
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 class PainterWrapperTest {
 
@@ -24,5 +25,28 @@ class PainterWrapperTest {
     @Test
     fun testDraw() {
         // TODO test: Screenshot test or draw to Bitmap, then compare Bitmap
+    }
+
+    @Test
+    fun testEqualsAndHashCode() {
+        val element1 = PainterWrapper(ColorPainter(Color.Red))
+        val element11 = PainterWrapper(ColorPainter(Color.Red))
+        val element2 = PainterWrapper(ColorPainter(Color.Green))
+
+        assertEquals(expected = element1, actual = element11)
+        assertNotEquals(illegal = element1, actual = element2)
+        assertNotEquals(illegal = element1, actual = null as Any?)
+        assertNotEquals(illegal = element1, actual = Any())
+
+        assertEquals(expected = element1.hashCode(), actual = element11.hashCode())
+        assertNotEquals(illegal = element1.hashCode(), actual = element2.hashCode())
+    }
+
+    @Test
+    fun testToString() {
+        assertEquals(
+            expected = "PainterWrapper(painter=ColorPainter(color=-65536))",
+            actual = PainterWrapper(ColorPainter(Color.Red)).toString()
+        )
     }
 }
