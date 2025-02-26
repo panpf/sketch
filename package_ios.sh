c2edb3db2d8296f112aa6b7ee7146128c5d21243
panpf <panpfpanpf@outlook.com>
2025-02-26 16:25:05 +0800

    build: ios package script to modify output location

--- package_ios.sh
+++ package_ios.sh
@@ -7,6 +7,6 @@ if [ "$1" != "--skipClean" ]; then
   ./gradlew clean
 fi
 
-xcodebuild -project sample/iosApp/iosApp.xcodeproj -scheme iosApp -destination 'platform=iOS Simulator,name=iPhone 16,OS=latest' CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO
+xcodebuild -project sample/iosApp/iosApp.xcodeproj -scheme iosApp -destination 'platform=iOS Simulator,name=iPhone 16,OS=latest' CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO -derivedDataPath sample/build/ios/outputs/
 
-echo "✅  iOS package is created successfully."
+echo "✅  iOS package is created successfully. $(pwd)/sample/build/ios/outputs/Build/Products/Debug-iphonesimulator/"
