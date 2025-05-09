fbd961fe2b0e7be6f33d3a638d547213b7d9adf3
panpf <panpfpanpf@outlook.com>
2025-05-09 15:33:37 +0800

    sample: Fixed bug where the example App failed to load on the Pexels page on Android 5.0 device

--- sample/src/androidMain/kotlin/com/github/panpf/sketch/sample/MyApplication.kt
+++ sample/src/androidMain/kotlin/com/github/panpf/sketch/sample/MyApplication.kt
@@ -16,6 +16,7 @@
 
 package com.github.panpf.sketch.sample
 
+import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import com.github.panpf.sketch.SingletonSketch
@@ -23,10 +24,21 @@ import com.github.panpf.sketch.Sketch
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 class MyApplication : Application(), SingletonSketch.Factory {
 
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        handleSSLHandshake()
+    }
 
     override fun createSketch(context: Context): Sketch {
         return newSketch(context)
@@ -35,5 +47,41 @@ class MyApplication : Application(), SingletonSketch.Factory {
     override fun onTerminate() {
         super.onTerminate()
         coroutineScope.cancel()
+    }
+
+    /**
+     * for api.pexels.com on Android 5.0
+     */
+    private fun handleSSLHandshake() {
+        try {
+            val trustAllCerts = arrayOf<TrustManager>(
+                @SuppressLint("CustomX509TrustManager")
+                object : X509TrustManager {
+                    override fun getAcceptedIssuers(): Array<X509Certificate?> {
+                        return arrayOfNulls(0)
+                    }
+
+                    @SuppressLint("TrustAllX509TrustManager")
+                    override fun checkClientTrusted(
+                        certs: Array<X509Certificate?>?,
+                        authType: String?
+                    ) {
+                    }
+
+                    @SuppressLint("TrustAllX509TrustManager")
+                    override fun checkServerTrusted(
+                        certs: Array<X509Certificate?>?,
+                        authType: String?
+                    ) {
+                    }
+                })
+            val sc = SSLContext.getInstance("TLS")
+            // trustAllCerts trust all certificates
+            sc.init(null, trustAllCerts, SecureRandom())
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.socketFactory)
+            HttpsURLConnection.setDefaultHostnameVerifier { _, _ -> true }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
