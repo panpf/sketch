87425c3ffd422ad00ca5fdaba3e75b98d854f8a0
panpf <panpfpanpf@outlook.com>
2025-06-24 15:51:30 +0800

    sample: Introducing koin dependency injection

--- sample/src/wasmJsMain/kotlin/com/github/panpf/sketch/sample/main.kt
+++ sample/src/wasmJsMain/kotlin/com/github/panpf/sketch/sample/main.kt
@@ -3,22 +3,12 @@ package com.github.panpf.sketch.sample
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import com.github.panpf.sketch.PlatformContext
-import com.github.panpf.sketch.SingletonSketch
 import com.github.panpf.sketch.sample.ui.App
-import org.koin.core.context.startKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    initials()
+    initialApp(PlatformContext.INSTANCE)
     CanvasBasedWindow("SketchSample-WASM") {
         App()
     }
-}
-
-private fun initials() {
-    startKoin {
-        modules(commonModule(PlatformContext.INSTANCE))
-        modules(platformModule(PlatformContext.INSTANCE))
-    }
-    SingletonSketch.setSafe { newSketch(it) }
 }
