275d795f1bfe8d8ecd9f53d52552b11a002d19fc
panpf <panpfpanpf@outlook.com>
2026-05-06 15:59:49 +0800

    build: Now supports checking binary compatibility before publishing, but it does not support the android platform yet

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.abi.AbiValidationExtension
+import org.jetbrains.kotlin.gradle.dsl.abi.AbiValidationMultiplatformExtension
+import org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.TestExecutable
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
@@ -149,13 +153,9 @@ allprojects {
     }
 
     // Configure publish plugin for all publishable library modules
-    if (
-//        && hasProperty("mavenCentralUsername")    // configured in the ~/.gradle/gradle.properties file
-//        && hasProperty("mavenCentralPassword")    // configured in the ~/.gradle/gradle.properties file
-        hasProperty("versionName")    // configured in the rootProject/gradle.properties file
-        && hasProperty("GROUP")    // configured in the rootProject/gradle.properties file
-        && hasProperty("POM_ARTIFACT_ID")    // configured in the project/gradle.properties file
-    ) {
+    val isPublishableModule =
+        hasProperty("POM_ARTIFACT_ID")    // configured in the project/gradle.properties file
+    if (isPublishableModule) {
         apply { plugin("com.vanniktech.maven.publish") }
 
         configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
@@ -177,7 +177,83 @@ allprojects {
     }
 
     // Configure Dokka plugin for all publishable library modules
-    if (hasProperty("POM_ARTIFACT_ID")) {   // configured in the module/gradle.properties file
+    if (isPublishableModule) {
         apply { plugin("org.jetbrains.dokka") }
     }
+
+    // Enable ABI validation for all publishable library modules.
+    if (isPublishableModule) {
+        configureAbiValidation()
+        configureAndroidLegacyAbiValidation()
+    }
+}
+
+@OptIn(ExperimentalAbiValidation::class)
+fun Project.configureAbiValidation() {
+    afterEvaluate {
+        val kotlinExtension =
+            extensions.findByType<KotlinProjectExtension>() ?: return@afterEvaluate
+
+        // Unfortunately the 'enabled' property doesn't share a common interface.
+        kotlinExtension.extensions.findByType<AbiValidationExtension>()?.apply {
+            enabled.set(true)
+        }
+        kotlinExtension.extensions.findByType<AbiValidationMultiplatformExtension>()?.apply {
+            enabled.set(true)
+        }
+    }
+}
+
+fun Project.configureAndroidLegacyAbiValidation() {
+    // TODO doesn't work. api files will not be generated for android platform
+    plugins.withId("com.android.kotlin.multiplatform.library") {
+        afterEvaluate {
+            tasks
+                .matching { it.name == "dumpLegacyAbi" || it.name.startsWith("dumpLegacyAbi") }
+                .configureEach {
+                    tasks.findByName("compileAndroidMain")?.let { dependsOn(it) }
+                    configureAndroidLegacyAbiDumpInput(this@configureAndroidLegacyAbiValidation)
+                }
+        }
+    }
+}
+
+private fun Task.configureAndroidLegacyAbiDumpInput(project: Project) {
+    val dumpTaskClass = javaClass
+    val getJvm = runCatching { dumpTaskClass.getMethod("getJvm") }.getOrNull() ?: return
+    val jvmProperty = getJvm.invoke(this)
+
+    val propertyClass = jvmProperty.javaClass
+    val get = propertyClass.getMethod("get")
+    val set = propertyClass.getMethod("set", Iterable::class.java)
+
+    val existingEntries = mutableListOf<Any>()
+    val currentEntries = get.invoke(jvmProperty)
+    if (currentEntries is Iterable<*>) {
+        currentEntries.filterNotNullTo(existingEntries)
+    } else {
+        return
+    }
+
+    val entryClass = existingEntries.firstOrNull()?.javaClass
+        ?: runCatching {
+            dumpTaskClass.classLoader.loadClass(
+                $$"org.jetbrains.kotlin.gradle.tasks.abi.KotlinLegacyAbiDumpTaskImpl$JvmTargetInfo",
+            )
+        }.getOrNull()
+        ?: return
+
+    val getSubdirectoryName = entryClass.getMethod("getSubdirectoryName")
+    if (existingEntries.any { getSubdirectoryName.invoke(it) == "android" }) return
+
+    val constructor = entryClass.getConstructor(String::class.java, FileCollection::class.java)
+    val androidClasses = project.files(
+        project.layout.buildDirectory.dir("classes/kotlin/android/main"),
+        project.layout.buildDirectory.dir("classes/java/android/main"),
+        project.layout.buildDirectory.dir("intermediates/javac/androidMain/classes"),
+    )
+    val androidEntry = constructor.newInstance("android", androidClasses)
+
+    existingEntries.add(androidEntry)
+    set.invoke(jvmProperty, existingEntries)
 }
