198114aa26e87b33349d2a497164869cf931665f
panpf <panpfpanpf@outlook.com>
2026-05-17 22:32:30 +0800

    sample: Fix wasmJs packaging failed due to awaitSkiko

--- samples/shared/src/wasmJsMain/kotlin/com/github/panpf/sketch/sample/util/WebWorker.wasmJs.kt
+++ samples/shared/src/wasmJsMain/kotlin/com/github/panpf/sketch/sample/util/WebWorker.wasmJs.kt
@@ -5,4 +5,4 @@ import kotlinx.coroutines.await
 @OptIn(ExperimentalWasmJsInterop::class)
 @Suppress("INVISIBLE_REFERENCE")
 internal actual suspend fun awaitSkiko(): JsAny =
-    org.jetbrains.skiko.wasm.awaitSkiko.await()
+    org.jetbrains.skiko.wasm.awaitSkiko.await()!!
