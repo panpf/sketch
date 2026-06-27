8960ac424d60da9319640cf39b7b2191bc0a2bb7
panpf <panpfpanpf@outlook.com>
2026-06-27 17:11:31 +0800

    build: Update actions workflow

--- internal/package_ios.sh
+++ internal/package_ios.sh
@@ -7,6 +7,10 @@ rootDir="${PWD%/internal*}"
 cd "$rootDir"
 
 ./gradlew clean
-xcodebuild -project samples/iosApp/iosApp.xcodeproj -scheme iosApp -destination 'generic/platform=iOS Simulator' ARCHS=arm64 CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO -derivedDataPath samples/shared/build/ios/outputs/
+xcodebuild \
+  -project samples/iosApp/iosApp.xcodeproj \
+  -scheme iosApp \
+  -destination 'generic/platform=iOS Simulator' ARCHS=arm64 CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO \
+  -derivedDataPath samples/shared/build/ios/outputs/
 
 echo "✅  iOS package is created successfully. $(pwd)/samples/shared/build/ios/outputs/Build/Products/Debug-iphonesimulator/"
