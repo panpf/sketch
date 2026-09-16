7b4307a05e114ff05bd86ce95ef0ffe66ce6f2cc
panpf <panpfpanpf@outlook.com>
2026-09-16 13:08:34 +0800

    sample: The example app now uses a stable disk cache directory.

--- samples/shared/src/desktopMain/kotlin/com/github/panpf/sketch/sample/initials.desktop.kt
+++ samples/shared/src/desktopMain/kotlin/com/github/panpf/sketch/sample/initials.desktop.kt
@@ -2,13 +2,16 @@ package com.github.panpf.sketch.sample
 
 import com.github.panpf.sketch.PlatformContext
 import com.github.panpf.sketch.Sketch
+import com.github.panpf.sketch.cache.DiskCache
 import com.github.panpf.sketch.fetch.supportHurlHttpUri
 import com.github.panpf.sketch.fetch.supportKtorHttpUri
 import com.github.panpf.sketch.fetch.supportOkHttpHttpUri
 import com.github.panpf.sketch.sample.util.PexelsCompatibleInterceptor
+import com.github.panpf.sketch.util.AppDirs
 import com.github.panpf.sketch.util.HurlHttpComponentProvider
 import com.github.panpf.sketch.util.KtorHttpComponentProvider
 import com.github.panpf.sketch.util.OkHttpHttpComponentProvider
+import okio.Path.Companion.toOkioPath
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.dsl.KoinAppDeclaration
@@ -30,6 +33,17 @@ actual fun platformModule(context: PlatformContext): Module = module {
 }
 
 actual fun Sketch.Builder.platformSketchInitial(context: PlatformContext) {
+    downloadCacheOptions {
+        DiskCache.Options(
+            appCacheDirectory = AppDirs.getCacheDir("SketchImageLoaderSample").toOkioPath()
+        )
+    }
+    resultCacheOptions {
+        DiskCache.Options(
+            appCacheDirectory = AppDirs.getCacheDir("SketchImageLoaderSample").toOkioPath()
+        )
+    }
+
     addIgnoredComponentProvider(
         KtorHttpComponentProvider::class,
         OkHttpHttpComponentProvider::class,
