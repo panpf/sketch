0692847e5d6c5c968eec32bd5fed8e675527bc88
panpf <panpfpanpf@outlook.com>
2026-09-20 02:07:55 +0800

    build: A new generate.sh file has been added to actively generate dependency classes.

---
+++ internal/generates.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+rootDir="${PWD%/internal*}"
+cd "$rootDir"
+
+./gradlew generateBuildKonfig
+./gradlew generateComposeResClass
